025972ed621a4a722d9c6aa6b0439a393e2a5184
Lala-banners <lala.l.banno@hotmail.com>
2021-11-17 13:14:44 +1100

    Made a swirly PS

--- Assets/VR Framework/FairyDustVR/Scripts/Wisp.cs
+++ Assets/VR Framework/FairyDustVR/Scripts/Wisp.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,15 +9,38 @@ namespace FDVR
     public class Wisp : MonoBehaviour
     {
         //Assign a GameObject in the Inspector to rotate around
-        public GameObject target;
+        private GameObject target;
 
         //How fast the object circles the target
         [SerializeField] private float rotationSpeed = 150;
 
+        [SerializeField] private float playerDist;
+
+        private Animator wispAnimator;
+
+        private void Start()
+        {
+            wispAnimator = GetComponent<Animator>();
+            target = GameObject.FindGameObjectWithTag("Player");
+        }
+
         void Update()
         {
             // Spin the object around the target at desired degrees/second.
-            transform.RotateAround(target.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+            //transform.RotateAround(target.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+
+            /*if (Vector3.Distance(transform.position, target.transform.position) < playerDist)
+            {
+                StartCoroutine(WispAnimation());
+            }*/
+
+            wispAnimator.SetBool("IsIdle", !Input.GetKeyDown(KeyCode.A));
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, playerDist);
         }
     }
 }
