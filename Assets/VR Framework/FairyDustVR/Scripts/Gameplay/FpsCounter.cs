a0cd122ae7925d297bcd84670cf2f0f99e3ebb2b
Lala-banners <lala.l.banno@hotmail.com>
2021-11-18 13:17:28 +1100

    Added snow globe to scene

--- Assets/VR Framework/FairyDustVR/Scripts/Gameplay/FpsCounter.cs
+++ Assets/VR Framework/FairyDustVR/Scripts/Gameplay/FpsCounter.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FpsCounter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_Text fpsText;
+    private int fps;
 
     // Update is called once per frame
     void Update()
     {
-        
+        fps = (int)(1 / Time.unscaledDeltaTime);
+        fpsText.text = "" + fps;
     }
 }
