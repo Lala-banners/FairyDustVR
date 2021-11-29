162ec8b1603944da63e77668e5d7bfc36c012f08
Lala-banners <lala.l.banno@hotmail.com>
2021-11-29 16:29:06 +1100

    yjsfgkdbsgfkuygkuyr

--- Assets/VR Framework/Scripts/Locomotion/VrMovement.cs
+++ Assets/VR Framework/Scripts/Locomotion/VrMovement.cs
@@ -4,26 +4,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 using VRFramework.Input;
+using UnityEditor;
+using UnityEngine.UI;
 
 namespace VRFramework
 {
     public class VrMovement : MonoBehaviour
     {
         public float speed;
+        public float jumpForce;
+        //Vector3 moveDirection = Vector3.zero;
+        Rigidbody rb;
 
-        private void Update()
+        private void Start()
         {
-            if (UnityEngine.Input.GetButton("Jump"))
-            {
-                
-            }
+            rb = GetComponent<Rigidbody>();
+        }
+
+        public void Jump()
+        {
+            rb.AddForce(Vector3.up * jumpForce);
         }
 
         public void Move()
         {
             transform.Translate(Vector3.forward * speed);
+            
+            /*Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            float curSpeedX = UnityEngine.Input.GetAxis("Vertical");
+            float curSpeedY = UnityEngine.Input.GetAxis("Horizontal");
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);*/
         }
-        
-        
     }
 }
