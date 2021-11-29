68727a40617d21e2e093ea08269be98d8c8c0f48
Lala-banners <lala.l.banno@hotmail.com>
2021-11-30 02:50:15 +1100

    Movement almost works

--- Assets/VR Framework/FairyDustVR/Scripts/Gameplay/TeleportPlayer.cs
+++ Assets/VR Framework/FairyDustVR/Scripts/Gameplay/TeleportPlayer.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class TeleportPlayer : MonoBehaviour
 {
     public GameObject player;
+    public Transform teleport;
 
     public void TeleportToPoint()
     {
         player.transform.position = new Vector3(200f, 1f, 250f);
+        player.transform.rotation = teleport.transform.rotation;
     }
 }
--- Assets/VR Framework/Scripts/Locomotion/VrMovement.cs
+++ Assets/VR Framework/Scripts/Locomotion/VrMovement.cs
@@ -1,38 +1,13 @@
+using System;
 using UnityEngine;
 using System.Collections;
 
 public class VrMovement : MonoBehaviour
 {
-    public float speed;
-    public float jumpForce;
-    Rigidbody rb;
-    private CharacterController _controller;
-    private Vector3 moveDirection;
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        _controller = GetComponent<CharacterController>();
-    }
-
-    public void Jump()
-    {
-        rb.AddForce(Vector3.up * jumpForce);
-    }
+    public float speed = 5f;
 
     public void MovePlayer()
     {
-        if (_controller.isGrounded)
-        {
-            Vector3 forward = transform.TransformDirection(Vector3.forward);
-            Vector3 right = transform.TransformDirection(Vector3.right);
-
-            float curSpeedX = speed * Input.GetAxis("Vertical");
-            float curSpeedY = speed * Input.GetAxis("Horizontal");
-
-            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-        }
-
-        _controller.Move(moveDirection * Time.deltaTime);
+        transform.Translate(Vector3.forward * speed);
     }
 }
